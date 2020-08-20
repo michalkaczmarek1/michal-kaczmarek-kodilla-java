6068270d5fc8e64b68e0bb7014ddb46cd76c78d4
Michał Kaczmarek <mi123456ka@gmail.com>
2020-08-20 20:46:34 +0200

    7.1 Beautifier text

---
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -0,0 +1,39 @@
+package com.kodilla.stream;
+
+import com.kodilla.stream.beautifier.PoemBeautifier;
+import com.kodilla.stream.lambda.ExpressionExecutor;
+import com.kodilla.stream.reference.FunctionalCalculator;
+
+
+public class StreamMain {
+    public static void main(String[] args) {
+        ExpressionExecutor expressionExecutor = new ExpressionExecutor();
+
+        System.out.println("Calculating expressions with lambdas");
+        expressionExecutor.executeExpression(10, 5, (a, b) -> a + b);
+        expressionExecutor.executeExpression(10, 5, (a, b) -> a - b);
+        expressionExecutor.executeExpression(10, 5, (a, b) -> a * b);
+        expressionExecutor.executeExpression(10, 5, (a, b) -> a / b);
+
+        System.out.println("Calculating expressions with method references");
+        expressionExecutor.executeExpression(3, 4, FunctionalCalculator::multiplyAByB);
+        expressionExecutor.executeExpression(3, 4, FunctionalCalculator::addAToB);
+        expressionExecutor.executeExpression(3, 4, FunctionalCalculator::subBFromA);
+        expressionExecutor.executeExpression(3, 4, FunctionalCalculator::divideAByB);
+
+        //Task 7.1
+        PoemBeautifier poemBeautifier = new PoemBeautifier();
+
+        String example1 = poemBeautifier.beautify("To jest przyklad", (text) -> text.toUpperCase() );
+        String example2 = poemBeautifier.beautify("To jest test", (text) -> text + "ABC" );
+        String example3 = poemBeautifier.beautify("To jest dla zadania", (text) -> "ABC" + text );
+        String example4 = poemBeautifier.beautify("TO JEST PRZYKLAD", (text) -> text.toLowerCase() );
+        String example5 = poemBeautifier.beautify("To jest przypadek testowy", (text) -> text.toUpperCase() + "/CBA/123" );
+
+        System.out.println(example1);
+        System.out.println(example2);
+        System.out.println(example3);
+        System.out.println(example4);
+        System.out.println(example5);
+    }
+}
