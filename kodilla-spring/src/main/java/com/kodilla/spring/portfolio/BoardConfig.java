70748ca7a40d8edc63dd05c73a24517255aaebe0
Michal Kaczmarek <mi123456ka@gmail.com>
2020-09-14 21:17:27 +0200

    10.3 Corrected task

--- kodilla-spring/src/main/java/com/kodilla/spring/portfolio/BoardConfig.java
+++ kodilla-spring/src/main/java/com/kodilla/spring/portfolio/BoardConfig.java
@@ -2,24 +2,33 @@ package com.kodilla.spring.portfolio;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
 
 @Configuration
 public class BoardConfig {
     @Bean
     public Board createBoard() {
-        return new Board(createToDoList(), createInProgressList(), createDoneList());
+        TaskList toDoList = createToDoList();
+        TaskList inProgressList = createInProgressList();
+        TaskList doneList = createDoneList();
+
+        return new Board(toDoList, inProgressList, doneList);
     }
+
     @Bean
+    @Scope("prototype")
     public TaskList createToDoList() {
         return new TaskList();
     }
 
     @Bean
+    @Scope("prototype")
     public TaskList createInProgressList() {
         return new TaskList();
     }
 
     @Bean
+    @Scope("prototype")
     public TaskList createDoneList() {
         return new TaskList();
     }
