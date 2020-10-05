f35e334db8d10d84caed179d118750a2c9484de4
Michal Kaczmarek <mi123456ka@gmail.com>
2020-10-05 20:42:14 +0200

    13.2 Correct task

--- kodilla-hibernate/src/main/java/com/kodilla/hibernate/tasklist/dao/TaskListDao.java
+++ kodilla-hibernate/src/main/java/com/kodilla/hibernate/tasklist/dao/TaskListDao.java
@@ -11,4 +11,6 @@ import java.util.List;
 @Repository
 public interface TaskListDao extends CrudRepository<TaskList, Integer> {
     List<TaskList> findByListName(String listName);
+
+    List<TaskList> findByDescription(String description);
 }
--- kodilla-hibernate/src/test/java/com/kodilla/hibernate/tasklist/dao/TaskListDaoTestSuite.java
+++ kodilla-hibernate/src/test/java/com/kodilla/hibernate/tasklist/dao/TaskListDaoTestSuite.java
@@ -4,8 +4,9 @@ import com.kodilla.hibernate.tasklist.TaskList;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -15,6 +16,23 @@ public class TaskListDaoTestSuite {
 
     @Autowired
     private TaskListDao taskListDao;
+
+    @Test
+    void testTaskListDaoSave() {
+        //Given
+        TaskList taskList = new TaskList("In progress", DESCRIPTION);
+
+        //When
+        taskListDao.save(taskList);
+
+        //Then
+        int id = taskList.getId();
+        Optional<TaskList> readTask = taskListDao.findById(id);
+        assertTrue(readTask.isPresent());
+
+        //clean up
+        taskListDao.deleteById(id);
+    }
 
     @Test
     void testFindByListName() {
@@ -34,5 +52,47 @@ public class TaskListDaoTestSuite {
         int id = readTaskLists.get(0).getId();
         taskListDao.deleteById(id);
 
+    }
+
+    @Test
+    void testFindByDescription() {
+        //Given
+        TaskList taskList = new TaskList("Done", DESCRIPTION);
+        taskListDao.save(taskList);
+
+        //When
+        List<TaskList> taskListsDesc = taskListDao.findByDescription(DESCRIPTION);
+
+        //Then
+        assertEquals(1, taskListsDesc.size());
+
+        //clean up
+        int id = taskListsDesc.get(0).getId();
+        taskListDao.deleteById(id);
+    }
+
+    @Test
+    void testFindAll() {
+        //Given
+        TaskList taskList1 = new TaskList("To do", DESCRIPTION);
+        TaskList taskList2 = new TaskList("In progress", DESCRIPTION);
+        taskListDao.save(taskList1);
+        taskListDao.save(taskList2);
+
+        //When
+        Iterable<TaskList> readTasks = taskListDao.findAll();
+        List<TaskList> tasksLists = new ArrayList<>();
+        for(TaskList taskList: readTasks) {
+            tasksLists.add(taskList);
+        }
+
+        //Then
+        assertEquals(2, tasksLists.size());
+
+        //clean up
+        int id1 = tasksLists.get(0).getId();
+        int id2 = tasksLists.get(1).getId();
+        taskListDao.deleteById(id1);
+        taskListDao.deleteById(id2);
     }
 }
