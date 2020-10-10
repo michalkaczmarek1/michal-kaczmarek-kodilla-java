5e6d894d3e8a34cbd1f580e4516054d8ae78dd7e
Michal Kaczmarek <mi123456ka@gmail.com>
2020-10-10 14:10:03 +0200

    13.4 Correct task

--- kodilla-hibernate/src/test/java/com/kodilla/hibernate/manytomany/dao/CompanyDaoTestSuite.java
+++ kodilla-hibernate/src/test/java/com/kodilla/hibernate/manytomany/dao/CompanyDaoTestSuite.java
@@ -59,7 +59,7 @@ class CompanyDaoTestSuite {
     }
 
     @Test
-    void testNamedQuery() {
+    void testNamedQueryCompany() {
         //Given
         Employee johnSmith = new Employee("John", "Smith");
         Employee stephanieClarckson = new Employee("Stephanie", "Clarckson");
@@ -87,12 +87,103 @@ class CompanyDaoTestSuite {
 
 
         //When
-        List<Employee> retrieveLastnameEmployees = employeeDao.retrieveEmployeeEqualLastname(johnSmith.getLastname());
         List<Company> retrieveNameCompaniesAfterFirstThreeCharacters = companyDao.retrieveComapniesFirstThreeCharacters("Sof");
 
         //Then
-        assertEquals(1, retrieveLastnameEmployees.size());
         assertEquals(1, retrieveNameCompaniesAfterFirstThreeCharacters.size());
+
+        // clean up
+        companyDao.deleteAll();
+    }
+
+    @Test
+    void testNamedQueryCompanyEmpty() {
+        //Given
+        Employee johnSmith = new Employee("John", "Smith");
+        Employee stephanieClarckson = new Employee("Stephanie", "Clarckson");
+
+        Company softwareMachine = new Company("Software Machine");
+
+        softwareMachine.getEmployees().add(johnSmith);
+        softwareMachine.getEmployees().add(stephanieClarckson);
+
+
+        johnSmith.getCompanies().add(softwareMachine);
+        stephanieClarckson.getCompanies().add(softwareMachine);
+
+        companyDao.save(softwareMachine);
+
+        //When
+        companyDao.delete(softwareMachine);
+        List<Company> retrieveNameCompaniesAfterFirstThreeCharacters = companyDao.retrieveComapniesFirstThreeCharacters("Sof");
+
+        //Then
+        assertEquals(0, retrieveNameCompaniesAfterFirstThreeCharacters.size());
+
+        // clean up
+        employeeDao.deleteAll();
+    }
+
+    @Test
+    void testNamedQueryEmployee() {
+        //Given
+        Employee johnSmith = new Employee("John", "Smith");
+        Employee stephanieClarckson = new Employee("Stephanie", "Clarckson");
+        Employee lindaKovalsky = new Employee("Linda", "Kovalsky");
+
+        Company softwareMachine = new Company("Software Machine");
+        Company dataMaesters = new Company("Data Maesters");
+        Company greyMatter = new Company("Grey Matter");
+
+        softwareMachine.getEmployees().add(johnSmith);
+        dataMaesters.getEmployees().add(stephanieClarckson);
+        dataMaesters.getEmployees().add(lindaKovalsky);
+        greyMatter.getEmployees().add(johnSmith);
+        greyMatter.getEmployees().add(lindaKovalsky);
+
+        johnSmith.getCompanies().add(softwareMachine);
+        johnSmith.getCompanies().add(greyMatter);
+        stephanieClarckson.getCompanies().add(dataMaesters);
+        lindaKovalsky.getCompanies().add(dataMaesters);
+        lindaKovalsky.getCompanies().add(greyMatter);
+
+        companyDao.save(softwareMachine);
+        companyDao.save(dataMaesters);
+        companyDao.save(greyMatter);
+
+        //When
+        List<Employee> retrieveLastnameEmployees = employeeDao.retrieveEmployeeEqualLastname(johnSmith.getLastname());
+
+        //Then
+        assertEquals(1, retrieveLastnameEmployees.size());
+
+        // clean up
+        companyDao.deleteAll();
+    }
+
+    @Test
+    void testNamedQueryEmployeeEmpty() {
+        //Given
+        Employee johnSmith = new Employee("John", "Smith");
+        Employee stephanieClarckson = new Employee("Stephanie", "Clarckson");
+
+        Company softwareMachine = new Company("Software Machine");
+
+        softwareMachine.getEmployees().add(johnSmith);
+        softwareMachine.getEmployees().add(stephanieClarckson);
+
+
+        johnSmith.getCompanies().add(softwareMachine);
+        stephanieClarckson.getCompanies().add(softwareMachine);
+
+        companyDao.save(softwareMachine);
+
+        //When
+        employeeDao.delete(johnSmith);
+        List<Employee> retrieveLastnameEmployees = employeeDao.retrieveEmployeeEqualLastname(johnSmith.getLastname());
+
+        //Then
+        assertEquals(0, retrieveLastnameEmployees.size());
 
         // clean up
         companyDao.deleteAll();
